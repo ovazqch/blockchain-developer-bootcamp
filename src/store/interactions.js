f2048a1b9e7fde31835c03b6e771462672b5b699
ovazqch <o.vazquezch@outlook.com>
2022-02-23 12:15:50 -0400

    updateFeb23

---
+++ src/store/interactions.js
@@ -0,0 +1,15 @@
+import Web3 from 'web3'
+import { 
+	web3Loaded
+} from './actions'
+
+export const loadWeb3 = async (dispatch) => {
+  if(typeof window.ethereum!=='undefined'){
+    const web3 = new Web3(window.ethereum)
+    dispatch(web3Loaded(web3))
+    return web3
+  } else {
+    window.alert('Please install MetaMask')
+    window.location.assign("https://metamask.io/")
+  }
+}
